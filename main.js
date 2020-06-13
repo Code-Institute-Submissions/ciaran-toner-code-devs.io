a998e6ca7bff8f8ac7450b0529330a5209fa2f6b
Ciaran <ciaran_toner@icloud.com>
2020-06-13 02:36:02 +0100

    Completed mobile navbar with social icons & modified media queries

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ menuicon.addEventListener("click", () => {
 });
 
 
-// $(function() {
-//     $(window).on("scroll", function() {
-//         if($(window).scrollTop() > 200) {
-//             $(".navbar-custom").addClass("active-navbar");
-//         } else {
-//            $(".navbar-custom").removeClass("active-navbar");
-//         }
-//     });
-// });
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 200) {
+            $(".navbar-custom").addClass("active-navbar");
+        } else {
+           $(".navbar-custom").removeClass("active-navbar");
+        }
+    });
+});
 
 
   

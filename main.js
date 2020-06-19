f07241af67fe93afc161702faf2ff53a75ef2dee
Ciaran <ciaran_toner@icloud.com>
2020-06-19 16:09:16 +0100

    chnaged font size, line height & margins on the article page.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// src for javascript mobile menu Dev ed https://www.youtube.com/watch?v=H4MkGzoACpQ&t=1181s
+
 const menuicon = document.querySelector(".menuicon");
 const navMenu = document.querySelector(".nav-menu");
 const links = document.querySelectorAll(".nav-menu-item li");
@@ -5,20 +7,3 @@ const links = document.querySelectorAll(".nav-menu-item li");
 menuicon.addEventListener("click", () => {
     navMenu.classList.toggle("open");
 });
-
-
-// $(function() {
-//     $(window).on("scroll", function() {
-//         if($(window).scrollTop() > 200) {
-//             $("nav").addClass("active-navbar");
-//         } else {
-//            $("nav").removeClass("active-navbar");
-//         }
-//     });
-// });
-
-
-  
-// typewriiter
-//   var typeWritterh1 = document.getElementById('typeWritter');
-// typeWritterh1.addEventListener('AnimationEnd',function( event ) { typeWritterh1.style.display = 'none'; }, false);

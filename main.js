ef2de4964150d220eacf803b78645c52bfbd99fd
Ciaran <ciaran_toner@icloud.com>
2020-07-05 01:39:16 +0100

    removed font-awsome js file & modified css files

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// src for javascript mobile menu Dev ed https://www.youtube.com/watch?v=H4MkGzoACpQ&t=1181s
+// This codes not my original work. Mobile menu clip-path effect src Dev ed https://www.youtube.com/watch?v=H4MkGzoACpQ&t=1181s
 
 const mobileMenu = document.querySelector(".mobile-menu");
 const navMenu = document.querySelector(".nav-menu");
@@ -8,8 +8,8 @@ mobileMenu.addEventListener("click", () => {
     navMenu.classList.toggle("open");
 });
 
-//Mobile menu src https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_icon_js
+// This codes not my original work. Mobile menu hamburger src https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_icon_js
 
 function mobileMenuOpen(x) {
-    x.classList.toggle("change");
+    x.classList.toggle("transform");
   }
